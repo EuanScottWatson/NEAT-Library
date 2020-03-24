298db9791bd04e23a1cfc298d68d4b10521b7bc5
Euan Imperial <es1519@ic.ac.uk>
2020-03-24 21:13:59 +0000

    Changed configuration a bit to remove population size and make package-private.

--- src/neat/CONFIGURATION.java
+++ src/neat/CONFIGURATION.java
@@ -1,23 +1,14 @@
 package neat;
 
-public class CONFIGURATION {
+class CONFIGURATION {
 
-  public float C1 = 1.0f;
-  public float C2 = 1.0f;
-  public float C3 = 0.4f;
-  public float DISTANCE_THRESHOLD = 10.0f;
-  public float MUTATION_THRESHOLD = 0.9f;
-  public float ADD_CONNECTION_THRESHOLD = 0.05f;
-  public float ADD_NODE_THRESHOLD = 0.03f;
-  public float MUTATION_WITHOUT_CROSSOVER = 0.2f;
+  float C1 = 1.0f;
+  float C2 = 1.0f;
+  float C3 = 0.4f;
+  float DISTANCE_THRESHOLD = 10.0f;
+  float MUTATION_THRESHOLD = 0.9f;
+  float ADD_CONNECTION_THRESHOLD = 0.05f;
+  float ADD_NODE_THRESHOLD = 0.03f;
+  float MUTATION_WITHOUT_CROSSOVER = 0.2f;
 
-  private int populationSize;
-
-  public CONFIGURATION(int populationSize) {
-    this.populationSize = populationSize;
-  }
-
-  public int getPopulationSize() {
-    return populationSize;
-  }
 }
